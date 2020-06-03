d5fbf9896f9fdf4c9e06086b19e1e7da027c19f2
Arvils <arvils.apsitis@gmail.com>
2020-06-03 17:12:19 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TrafficLigths
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redLigth = new System.Windows.Forms.PictureBox();
+            this.yellowLigth = new System.Windows.Forms.PictureBox();
+            this.greenLigth = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.redLigth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowLigth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenLigth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // redLigth
+            // 
+            this.redLigth.Location = new System.Drawing.Point(12, 12);
+            this.redLigth.Name = "redLigth";
+            this.redLigth.Size = new System.Drawing.Size(130, 130);
+            this.redLigth.TabIndex = 0;
+            this.redLigth.TabStop = false;
+            // 
+            // yellowLigth
+            // 
+            this.yellowLigth.Location = new System.Drawing.Point(12, 152);
+            this.yellowLigth.Name = "yellowLigth";
+            this.yellowLigth.Size = new System.Drawing.Size(130, 130);
+            this.yellowLigth.TabIndex = 1;
+            this.yellowLigth.TabStop = false;
+            // 
+            // greenLigth
+            // 
+            this.greenLigth.Location = new System.Drawing.Point(12, 293);
+            this.greenLigth.Name = "greenLigth";
+            this.greenLigth.Size = new System.Drawing.Size(130, 130);
+            this.greenLigth.TabIndex = 2;
+            this.greenLigth.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(155, 435);
+            this.Controls.Add(this.greenLigth);
+            this.Controls.Add(this.yellowLigth);
+            this.Controls.Add(this.redLigth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.redLigth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowLigth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenLigth)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox redLigth;
+        private System.Windows.Forms.PictureBox yellowLigth;
+        private System.Windows.Forms.PictureBox greenLigth;
+    }
+}
+
